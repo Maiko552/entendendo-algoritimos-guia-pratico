1870235b7e1d7f1923742ff55931a6fe9b22bf7a
Maikon <mknsposito@gmail.com>
2025-08-19 13:57:19 -0300

    feat: exercicios 1.3 à 1.6 completados AgendaTelefonica

--- src/main/java/br/com/maikonspo/pesquisabinaria/AgendaTelefonica.java
+++ src/main/java/br/com/maikonspo/pesquisabinaria/AgendaTelefonica.java
@@ -11,7 +11,44 @@ public class AgendaTelefonica {
         agendaTelefonica.put(nome, tel);
     }
 
-    static void buscaDentroMap(String nome) {
+    static void agendaCompleta() {
+        System.out.printf("%-15s | %-15s%n", "NOME", "TELEFONE");
+        System.out.println("-----------------+-----------------");
+        for (Map.Entry<String, String> entry : agendaTelefonica.entrySet()) {
+            System.out.printf("%-15s | %-15s%n", entry.getKey(), entry.getValue());
+        }
+    }
+
+
+    static void buscaDentroMapPorValorAgendaCompleta(String tel){
+        for (Map.Entry<String, String> entry : agendaTelefonica.entrySet()) {
+            if (entry.getValue().equals(tel)) {
+                System.out.println("Telefone " + tel + " pertence a: " + entry.getKey());
+                return;
+            }
+        }
+        System.out.println("Telefone \"" + tel + "\" não encontrado.");
+    }
+
+    static void buscaDentroMapLetraA() {
+        System.out.printf("%-15s | %-15s%n", "NOME", "TELEFONE");
+        System.out.println("-----------------+-----------------");
+
+        boolean encontrou = false;
+        for (Map.Entry<String, String> entry : agendaTelefonica.entrySet()) {
+            if (entry.getKey().toUpperCase().startsWith("A")) {
+                System.out.printf("%-15s | %-15s%n", entry.getKey(), entry.getValue());
+                encontrou = true;
+            }
+        }
+
+        if (!encontrou) {
+            System.out.println("Nenhum contato iniciado com a letra A.");
+        }
+    }
+
+
+    static void buscaDentroMapPorChave(String nome) {
         String telefone = agendaTelefonica.get(nome);
         if (telefone != null) {
             System.out.println("Nome: " + nome + " | Telefone: " + telefone);
@@ -27,9 +64,16 @@ public class AgendaTelefonica {
         addAgenda("José", "574838593");
         addAgenda("João", "11839043040");
         addAgenda("Sarah", "57756545434");
+        addAgenda("Almerinda", "95832094244");
 
-        buscaDentroMap("Maikon");
-        buscaDentroMap("Sarah");
-        buscaDentroMap("Lucas");
+        buscaDentroMapPorChave("Maikon");
+        buscaDentroMapPorChave("Sarah");
+        buscaDentroMapPorChave("Lucas");
+        buscaDentroMapPorValorAgendaCompleta("57756545434");
+
+        agendaCompleta();
+
+        buscaDentroMapLetraA();
+
     }
 }
